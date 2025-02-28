f68bed01552eab0774d7cf291fd2944b85bb7aad
OmarElhagagy <oadeell21@gmail.com>
2025-02-28 21:38:52 +0200

    added getters and setters for customers JPA

--- src/main/java/com/yourstore/model/Customer.java
+++ src/main/java/com/yourstore/model/Customer.java
@@ -63,4 +63,116 @@ public class Customer {
 
 	@OneToMany(mappedBy = "customer")
 	private Set<Notifications> notifications = new HashSet<>();
+
+	public Integer getCustomerId() {
+		return customerId;
+	}
+
+	public void setCustomerId(Integer customerId) {
+		this.customerId = customerId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public LocalDate getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(LocalDate birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Set<Users> getUsers() {
+		return users;
+	}
+
+	public void setUsers(Set<Users> users) {
+		this.users = users;
+	}
+
+	public Set<Address> getAddresses() {
+		return addresses;
+	}
+
+	public void setAddresses(Set<Address> addresses) {
+		this.addresses = addresses;
+	}
+
+	public Set<CustomerOrder> getOrders() {
+		return orders;
+	}
+
+	public void setOrders(Set<CustomerOrder> orders) {
+		this.orders = orders;
+	}
+
+	public Set<Cart> getCartItems() {
+		return cartItems;
+	}
+
+	public void setCartItems(Set<Cart> cartItems) {
+		this.cartItems = cartItems;
+	}
+
+	public Set<WishList> getWhishListItmes() {
+		return whishListItmes;
+	}
+
+	public void setWhishListItmes(Set<WishList> whishListItmes) {
+		this.whishListItmes = whishListItmes;
+	}
+
+	public Set<PhoneNumbersCustomers> getPhoneNumbers() {
+		return phoneNumbers;
+	}
+
+	public void setPhoneNumbers(Set<PhoneNumbersCustomers> phoneNumbers) {
+		this.phoneNumbers = phoneNumbers;
+	}
+
+	public Set<Notifications> getNotifications() {
+		return notifications;
+	}
+
+	public void setNotifications(Set<Notifications> notifications) {
+		this.notifications = notifications;
+	}
 }
