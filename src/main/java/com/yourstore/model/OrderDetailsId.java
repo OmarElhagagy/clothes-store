e4f2b5620b4b587e3994ba44da5ed9131b12086f
OmarElhagagy <oadeell21@gmail.com>
2025-03-10 01:35:09 +0200

    removed unecessary import

--- src/main/java/com/yourstore/model/OrderDetailsId.java
+++ src/main/java/com/yourstore/model/OrderDetailsId.java
@@ -1,23 +1,9 @@
 package com.yourstore.model;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
-import java.util.HashSet;
-import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.MapsId;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Table;
-import jakarta.validation.constraints.Email;
 
 @Embeddable
 public class OrderDetailsId implements Serializable {
