c71073b9fb1b338d9e95d92cf11684aea51d144b
OmarElhagagy <oadeell21@gmail.com>
2025-03-09 23:43:01 +0200

    Completed Order_Details table with getters ,setters, equals, and hashCode

--- src/main/java/com/yourstore/model/OrderDetails.java
+++ src/main/java/com/yourstore/model/OrderDetails.java
@@ -1,15 +1,18 @@
 package com.yourstore.model;
 
+import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Set;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
@@ -29,4 +32,135 @@ public class OrderDetails {
 	@MapsId("productId")
 	@JoinColumn(name = "Product_ID")
 	private Product product;
+
+	@Column(name = "Quantity", nullable = false)
+	private Integer quantity;
+
+	@Column(name = "Original_Price", nullable= false, precision = 10, scale = 2)
+	private BigDecimal originalPrice;
+
+	@Column(name = "Discount_Amount", nullable = false, precision = 10, scale = 2)
+	private BigDecimal discountAmount;
+
+	@Column(name = "Final_Price", nullable = false, precision = 10, scale = 2)
+	private BigDecimal finalPrice;
+
+	// getters and setters
+	public OrderDetailsId getId() {
+		return id;
+	}
+
+	public void setId(OrderDetailsId id) {
+		this.id = id;
+	}
+
+	public CustomerOrder getOrder() {
+		return order;
+	}
+
+	public void setOrder(CustomerOrder order) {
+		this.order = order;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public BigDecimal getOriginalPrice() {
+		return originalPrice;
+	}
+
+	public void setOriginalPrice(BigDecimal originalPrice) {
+		this.originalPrice = originalPrice;
+	}
+
+	public BigDecimal getDiscountAmount() {
+		return discountAmount;
+	}
+
+	public void setDiscountAmount(BigDecimal discountAmount) {
+		this.discountAmount = discountAmount;
+	}
+
+	public BigDecimal getFinalPrice() {
+		return finalPrice;
+	}
+
+	public void setFinalPrice(BigDecimal finalPrice) {
+		this.finalPrice = finalPrice;
+	}
+
+	// equals and hashcode
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((order == null) ? 0 : order.hashCode());
+		result = prime * result + ((product == null) ? 0 : product.hashCode());
+		result = prime * result + ((quantity == null) ? 0 : quantity.hashCode());
+		result = prime * result + ((originalPrice == null) ? 0 : originalPrice.hashCode());
+		result = prime * result + ((discountAmount == null) ? 0 : discountAmount.hashCode());
+		result = prime * result + ((finalPrice == null) ? 0 : finalPrice.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderDetails other = (OrderDetails) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (order == null) {
+			if (other.order != null)
+				return false;
+		} else if (!order.equals(other.order))
+			return false;
+		if (product == null) {
+			if (other.product != null)
+				return false;
+		} else if (!product.equals(other.product))
+			return false;
+		if (quantity == null) {
+			if (other.quantity != null)
+				return false;
+		} else if (!quantity.equals(other.quantity))
+			return false;
+		if (originalPrice == null) {
+			if (other.originalPrice != null)
+				return false;
+		} else if (!originalPrice.equals(other.originalPrice))
+			return false;
+		if (discountAmount == null) {
+			if (other.discountAmount != null)
+				return false;
+		} else if (!discountAmount.equals(other.discountAmount))
+			return false;
+		if (finalPrice == null) {
+			if (other.finalPrice != null)
+				return false;
+		} else if (!finalPrice.equals(other.finalPrice))
+			return false;
+		return true;
+	}
 }
