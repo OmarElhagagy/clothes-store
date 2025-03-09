049be427a6a6bc78bdfa203c74e1b17160c10369
OmarElhagagy <oadeell21@gmail.com>
2025-03-10 00:00:54 +0200

    removed unecessary imports from Order_Details

---
+++ src/main/java/com/yourstore/model/OrderDetailsId.java
@@ -0,0 +1,21 @@
+package com.yourstore.model;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+
+@Embeddable
+public class OrderDetailsId implements se
