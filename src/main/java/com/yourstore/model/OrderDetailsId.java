1183f5585a36815cddd3a71a10a487610d4845f7
OmarElhagagy <oadeell21@gmail.com>
2025-03-10 00:41:10 +0200

    added OrderdetailsId with getters, setters, equals, and hashCode

--- src/main/java/com/yourstore/model/OrderDetailsId.java
+++ src/main/java/com/yourstore/model/OrderDetailsId.java
@@ -1,10 +1,12 @@
 package com.yourstore.model;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.Set;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -18,4 +20,59 @@ import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 
 @Embeddable
-public class OrderDetailsId implements se
+public class OrderDetailsId implements Serializable {
+	@Column(name = "Order_ID")
+	private Integer orderId;
+
+	@Column(name = "Product_ID")
+	private Integer productId;
+
+	//Getters and Setters
+	public Integer getOrderId() {
+		return orderId;
+	}
+
+	public void setOrderId(Integer orderId) {
+		this.orderId = orderId;
+	}
+
+	public Integer getProductId() {
+		return productId;
+	}
+
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+
+	// equals and hashCode
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((orderId == null) ? 0 : orderId.hashCode());
+		result = prime * result + ((productId == null) ? 0 : productId.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderDetailsId other = (OrderDetailsId) obj;
+		if (orderId == null) {
+			if (other.orderId != null)
+				return false;
+		} else if (!orderId.equals(other.orderId))
+			return false;
+		if (productId == null) {
+			if (other.productId != null)
+				return false;
+		} else if (!productId.equals(other.productId))
+			return false;
+		return true;
+	}
+}
