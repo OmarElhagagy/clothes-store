7c53a3bb4eb5cbec52515a7f310092d27e8351c3
OmarElhagagy <oadeell21@gmail.com>
2025-03-10 02:48:06 +0200

    added Price_History table

---
+++ src/main/java/com/yourstore/model/PriceHistory.java
@@ -0,0 +1,109 @@
+package com.yourstore.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Price_History")
+public class PriceHistory {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "History_ID")
+	private Integer historyId;
+
+	@ManyToOne
+	@JoinColumn(name = "Product_ID", nullable = false)
+	private Product product;
+
+	@Column(name = "Price", nullable = false, precision = 10, scale = 2)
+	private BigDecimal price;
+
+	@Column(name = "Change_Date", nullable = false)
+	private LocalDate changeDate;
+
+	// getters and setters
+	public Integer getHistoryId() {
+		return historyId;
+	}
+
+	public void setHistoryId(Integer historyId) {
+		this.historyId = historyId;
+	}
+
+	public Product getProduct() {
+		return product;
+	}
+
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public LocalDate getChangeDate() {
+		return changeDate;
+	}
+
+	public void setChangeDate(LocalDate changeDate) {
+		this.changeDate = changeDate;
+	}
+
+	// equals and hashCode
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((historyId == null) ? 0 : historyId.hashCode());
+		result = prime * result + ((product == null) ? 0 : product.hashCode());
+		result = prime * result + ((price == null) ? 0 : price.hashCode());
+		result = prime * result + ((changeDate == null) ? 0 : changeDate.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PriceHistory other = (PriceHistory) obj;
+		if (historyId == null) {
+			if (other.historyId != null)
+				return false;
+		} else if (!historyId.equals(other.historyId))
+			return false;
+		if (product == null) {
+			if (other.product != null)
+				return false;
+		} else if (!product.equals(other.product))
+			return false;
+		if (price == null) {
+			if (other.price != null)
+				return false;
+		} else if (!price.equals(other.price))
+			return false;
+		if (changeDate == null) {
+			if (other.changeDate != null)
+				return false;
+		} else if (!changeDate.equals(other.changeDate))
+			return false;
+		return true;
+	}
+}
